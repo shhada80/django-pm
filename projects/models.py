c230105fadd3bd186cd006a27ac93b773d42433e
shehada <shhada80@gmail.com>
2025-08-26 12:12:15 +0300

    Initiate project models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+# نموذج User يقدمه لنا جانغو ضمنياً؛ لتمثيل العلاقة بين المستخدم والمشروع
+from django.conf.global_settings import AUTH_USER_MODEL
+
 
 # Create your models here.
+
+# التصنيف
+# نعرف صنفاً جديداً خاص بكيان التصنيف
+class Category(models.Model):
+    # حقول النموذج
+    # اسم التصنيف
+    name = models.CharField(max_length=255)
+
+    # ستعيد لنا نتيجة عند تحويل الصنف إلى سلسلة نصية
+    def __str__(self):
+        return self.name
+
+
+# نموذج مساعد للمشروع خاص بحالة المشروع
+"""
+هذا النموذج لا يرث من Model بل يرث من صنف آخر يدعى IntegerChoices
+وبالتالي فهو عبارة عن نموذج مساعد لتمثيل حالة المشروع
+"""
+class ProjectStatus(models.IntegerChoices):
+    # سنستخدم ضمنه حقول الخيارات ل
+    # تعريف الحالات الموجودة لدينا
+    """
+    PENDING = 1, 'Pending'
+    Pending القيمة التي ستظهر للمستخدم عند تمثيل المشروع في الواجهة
+    بينما القيمة 1 هي التي ستخزن في قاعدة البيانات
+    أما كلمة PENDING يمكن استخدامها ضمن الشيفرة بدلاً من الرقم 1 للوضوح وتجنب الأخطاء
+    """
+    PENDING = 1, 'Pending' # قيد التنفيذ
+    COMPLETED = 2, 'Completed' # مكتمل
+    POSTPONED = 3, 'Postponed' # مؤجل
+    CANCELLED = 4, 'Cancelled' # ملغي
+
+# المشروع
+class Project(models.Model):
+    # خواص المشروع
+    # عنوان المشروع
+    title = models.CharField(max_length=255)
+    # وصف المشروع
+    description = models.TextField()
+    # إنشاء حقل يعبر عن الحالة من النوع Integer
+    # الخيارات المتاحة لهذا الحقل هي الخيارات من الصنف ProjectStatus الموجود في الأعلى
+    status = models.IntegerField(
+        choices=ProjectStatus.choices, # type: ignore
+        # في السطر السابق كتبنا: type: ignore؛ السبب: PyCharm أحياناً لا يتعرف تلقائياً على الخصائص الديناميكية التي ينشئها IntegerChoices
+        # الحالة الافتراضية: قيد التنفيذ
+        default=ProjectStatus.PENDING
+    )
+    # تاريخ الإنشاء
+    create_at = models.DateTimeField(auto_now_add=True) # auto_now_add: يضبط التاريخ عند الإنشاء فقط (مرة واحدة)
+    # تاريخ التعديل
+    update_at = models.DateTimeField(auto_now=True) # auto_now: يحدث التاريخ تلقائياً عند كل تعديل على النموذج
+
+    # تمثيل حقول العلاقات
+    # العلاقة بين النموذج Category والمشروع
+    # نوع العلاقة: واحد إلى كثير
+    # PROTECT: يمنع حذف التصنيف إذا كانت هناك مشروع مرتبط به
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    # العلاقة بين النموذج User والمشروع
+    # نوع العلاقة: واحد إلى كثير
+    # CASCADE: عند حذف المستخدم، تُحذف كل المشاريع المرتبطة به تلقائياً
+    """
+    لم ننشئ النموذج User بعد، ولسنا بحاجة لذلك فعلياً؛ حيث يقدم لنا جانغو هذا النموذج بشكل ضمني
+    وهو AUTH_USER_MODEL، ويمكن استخدامه هنا لتمثيل هذه العلاقة، وذلك بعد استيراده في الأعلى
+    """
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+    # لتمثيل الكائن بسلسلة نصية، وهي العنوان
+    def __str__(self):
+        return self.title
+
+
+# المهمة
+class Task(models.Model):
+    # خواص المهمة
+    # وصف المهمة
+    description = models.TextField()
+    # حالة المهمة: مكتملة أم لا؟
+    is_completed = models.BooleanField(default=False) # False: غير مكتملة، وعند إضافة مهمة تكون غير مكتملة بعد
+    # تمثيل حقول العلاقات
+    # العلاقة بين النموذج Project والمهام
+    # نوع العلاقة: واحد إلى كثير
+    # CASCADE: عند حذف المشروع، تُحذف كل المهام المرتبطة به تلقائياً
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    # لتمثيل الكائن بسلسلة نصية، وهي الوصف
+    def __str__(self):
+        return self.description
