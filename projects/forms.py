96307afcb6bbf295f8e44c5ddd7a96321cade530
shehada <shhada80@gmail.com>
2025-08-31 20:18:49 +0300

    Project management

--- projects/forms.py
+++ projects/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from . import models
 
-
+# إنشاء المشروع
 class ProjectCreateForm(forms.ModelForm):
     # التصريح عن الصنف Meta
     class Meta:
         # النموذج المستهدف
         model = models.Project
-        # تحديد الحقول المطلوبة في ههذ الاستمارة
+        # تحديد الحقول المطلوبة في هذه الاستمارة
         # لا نريد جميع الحقول لإدخالها، فحقل الحالة سيحدد تلقائياً عند إنشاء المشروع، وكذلك تاريخ الإنشاء والتحديث، والمستخدم صاحب المشروع
         fields = ['category', 'title', 'description']
         # تحديد الحقول
@@ -15,4 +15,19 @@ class ProjectCreateForm(forms.ModelForm):
             'category': forms.Select(),
             'title': forms.TextInput(),
             'description': forms.Textarea()
+        }
+
+# تعديل المشروع
+class ProjectUpdateForm(forms.ModelForm):
+    # التصريح عن الصنف Meta
+    class Meta:
+        # النموذج المستهدف
+        model = models.Project
+        # تحديد الحقول المطلوبة في هذه الاستمارة
+        fields = ['category', 'title', 'status']
+        # تحديد الحقول
+        widgets = {
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'status': forms.Select()
         }
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 # استيراد النماذج
 from . import models
 from . import forms
@@ -13,6 +13,7 @@ class ProjectListView(ListView):
     # القالب المرتبط مع هذه النافذة
     template_name = 'project/list.html'
 
+# إنشاء المشروع
 class ProjectCreateView(CreateView):
     # نحدد النموذج المرتبط مع هذه النافذة
     model = models.Project
@@ -23,3 +24,61 @@ class ProjectCreateView(CreateView):
     template_name = 'project/create.html'
     # رابط إعادة التوجيه، وهو الصفحة الرئيسية أو قائمة المشاريع
     success_url = reverse_lazy('Project_list')
+
+# تعديل المشروع
+class ProjectUpdateView(UpdateView):
+    # نحدد النموذج المرتبط مع هذه النافذة
+    model = models.Project
+    # نحدد الاستمارة
+    # نحدد النموذج المرتبط مع هذه النافذة
+    form_class = forms.ProjectUpdateForm
+    # القالب المرتبط مع هذه النافذة
+    template_name = 'project/update.html'
+    # رابط إعادة التوجيه، وهو الصفحة الرئيسية أو قائمة المشاريع
+    # success_url = reverse_lazy('Project_list')
+
+    # لإعادة التوجيه لصفحة المشروع ذاته
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.id])
+
+# حذف المشروع
+class ProjectDeleteView(DeleteView):
+    # نحدد النموذج المرتبط مع هذه النافذة
+    model = models.Project
+    # القالب المرتبط مع هذه النافذة
+    template_name = 'project/delete.html'
+    # رابط إعادة التوجيه، وهو الصفحة الرئيسية أو قائمة المشاريع
+    success_url = reverse_lazy('Project_list')
+
+# إنشاء مهمة
+class TaskCreateView(CreateView):
+    # نحدد النموذج المرتبط مع هذه النافذة
+    model = models.Task
+    # الحقول المطلوبة عند إنشاء المهمة
+    fields = ['project', 'description']
+    # نريد post فقط، ولا نريد عرض صفحة إنشاء المهمة
+    http_method_names = ['post']
+   # لإعادة التوجيه لصفحة المشروع
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+
+# تحديث مهمة
+class TaskUpdateView(UpdateView):
+    # نحدد النموذج المرتبط مع هذه النافذة
+    model = models.Task
+    # الحقول المطلوبة عند إنشاء المهمة
+    fields = ['is_completed']
+    # نريد post فقط، ولا نريد عرض صفحة إنشاء المهمة
+    http_method_names = ['post']
+   # لإعادة التوجيه لصفحة المشروع
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+
+# حذف مهمة
+class TaskDeleteView(DeleteView):
+    # نحدد النموذج المرتبط مع هذه النافذة
+    model = models.Task
+
+   # لإعادة التوجيه لصفحة المشروع
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
