b2c5edbe0b51785f4a7e4ec7e3daec024953ff00
shehada <shhada80@gmail.com>
2025-11-24 22:02:48 +0300

    Preparing the site for publishing

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -155,6 +155,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
